14beb076c2afc7bc968118c4032158a8c074ab43
Tiago Onofre <taraujonofre@gmail.com>
2019-09-04 23:50:44 -0300

    started

---
+++ include/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <chrono>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+
+void (*algorithms[8]) ( long int *first, long int *last );
+
+#endif
---
+++ include/selection.h
@@ -0,0 +1,6 @@
+#ifndef SELECTION_H
+#define SELECTION_H
+
+void selection_sort( long int *first , long int *last );
+
+#endif
